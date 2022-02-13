58b8fd430b3a6468dbec469dcc7703c7e74e3667
Yeliana Torres <yelis124@gmail.com>
2022-02-13 16:39:27 -0500

    turn some components into separate files

---
+++ src/molecules/NoResponseMessage.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Icon } from '@superys/momo-ui';
+
+function NoResponseMessage() {
+  return (
+    <NoResponseStyled>
+      <h4 className="subtitle">Enter a URL and click send to get a response</h4>
+      <Icon icon="butterfly" size={110} />
+    </NoResponseStyled>
+  );
+}
+
+const NoResponseStyled = styled.div`
+  text-align: center;
+  width: 90%;
+  max-width: 270px;
+  margin: 40% auto;
+  opacity: 50%;
+`;
+
+export default NoResponseMessage;

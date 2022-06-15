1d661f4a98254c980e0eccaac43a34c8cc92a097
Yeliana Torres <yelis124@gmail.com>
2022-06-15 11:13:09 -0500

    move molecules to typescript

---
+++ src/components/molecules/JSONEditor.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { JsonEditorProps } from "svelte-jsoneditor/components/JSONEditor.svelte";
+import { JSONEditor } from "svelte-jsoneditor/dist/jsoneditor.js";
+
+function SvelteJSONEditor(props: JsonEditorProps) {
+  const editor = useRef<HTMLDivElement>(null);
+
+  // create editor
+  // const onRef = (ref: React.MutableRefObject<HTMLDivElement>) => {
+  //   if (ref && !editor.current) {
+  //     editor.current = new JSONEditor({
+  //       target: ref,
+  //       props,
+  //     });
+  //   }
+  // };
+
+  // // update props
+  // useEffect(() => {
+  //   if (editor.current) {
+  //     editor.current.updateProps(props);
+  //   }
+  // }, [props]);
+
+  // // destroy editor
+  // useEffect(() => {
+  //   return () => {
+  //     if (editor.current) {
+  //       editor.current.destroy();
+  //       editor.current = null;
+  //     }
+  //   };
+  // }, []);
+
+  // return <StyeldEditor ref={onRef}></StyeldEditor>;
+  return <StyeldEditor>Here goes the editor</StyeldEditor>;
+}
+
+const StyeldEditor = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export default SvelteJSONEditor;
---
+++ src/components/molecules/NoResponseMessage.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { Icon, red, TertiaryButton } from "@superys/momo-ui";
+import { ErrorType } from "../../state";
+
+type NoResponseProps = {
+  error: ErrorType;
+  isLoading: boolean;
+  cancelRequest: () => void;
+};
+
+function NoResponseMessage(props: NoResponseProps) {
+  const icon = props.error ? "warning-circle" : "butterfly";
+  const text = props.error
+    ? props.error.message
+    : props.isLoading
+    ? "Sending request ..."
+    : "Enter a URL and click send to get a response";
+
+  return (
+    <NoResponseStyled error={props.error !== null} isLoading={props.isLoading}>
+      <h4 className="subtitle">{text}</h4>
+
+      {props.isLoading ? (
+        <TertiaryButton onClick={props.cancelRequest}>Cancel</TertiaryButton>
+      ) : (
+        <Icon icon={icon} size={110} />
+      )}
+    </NoResponseStyled>
+  );
+}
+
+const NoResponseStyled = styled.div<{ error: boolean; isLoading: boolean }>`
+  text-align: center;
+  width: 90%;
+  max-width: 270px;
+  margin: 30% auto;
+
+  ${({ error }) => (error ? `color: ${red[200]};` : "")}
+  ${({ isLoading, error }) => (!isLoading && !error ? "opacity: 50%;" : "")}
+`;
+
+export default NoResponseMessage;

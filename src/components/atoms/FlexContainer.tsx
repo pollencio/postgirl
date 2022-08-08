aa71a1687dca5fff37f100119300d6f611433860
Yeliana Torres <yelis124@gmail.com>
2022-08-08 11:00:41 -0500

    all changes

--- src/components/atoms/FlexContainer.tsx
+++ src/components/atoms/FlexContainer.tsx
@@ -9,12 +9,16 @@ type FlexContainerProps = {
     | 'space-around'
     | 'space-evenly';
   alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  gap?: number;
+  wrap?: boolean;
 };
 
 const FlexContainer = styled.div<FlexContainerProps>`
   display: flex;
   justify-content: ${(props) => props.justifyContent};
   align-items: ${(props) => props.alignItems};
+  gap: ${(props) => props.gap}px;
+  flex-wrap: ${(props) => (props.wrap ? 'wrap' : '')};
 `;
 
 export default FlexContainer;
--- src/modules/Request/components/RequestBody.tsx
+++ src/modules/Request/components/RequestBody.tsx
@@ -1,17 +1,19 @@
-import JSONEditor from '../../../components/molecules/JSONEditor';
-import useAppContext from '../../../state/useAppContext';
+import EditorMockup from '../../../components/molecules/EditorMockup';
+// import JSONEditor from '../../../components/molecules/JSONEditor';
+// import useAppContext from '../../../state/useAppContext';
 
 function Body() {
-  const { request, setRequestElement } = useAppContext();
+  // const { request, setRequestElement } = useAppContext();
 
   return (
-    <JSONEditor
-      content={request.data || {}}
-      mainMenuBar={false}
-      navigationBar={false}
-      mode="code"
-      onChange={(newData) => setRequestElement('data', newData)}
-    />
+    // <JSONEditor
+    //   content={request.data || {}}
+    //   mainMenuBar={false}
+    //   navigationBar={false}
+    //   mode="code"
+    //   onChange={(newData) => setRequestElement('data', newData)}
+    // />
+    <EditorMockup />
   );
 }
 
